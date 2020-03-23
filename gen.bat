243b7e2cba1b72cbf833d10c57ca0bac4341be0c
Pavel Krotkov <krotkov.pavel@gmail.com>
2020-03-22 19:21:38 -0700

    Move around dirs

--- gen.bat
+++ gen.bat
@@ -1,2 +1,3 @@
-thrift.exe --gen cpp:no_skeleton -out obs-thrift-api/src/ obs_thrift_server.thrift
-thrift.exe --gen py -out tests/ obs_thrift_server.thrift
+thrift.exe --gen cpp:no_skeleton -out obs-thrift-api/obs-thrift-api/src/ obs_thrift_server.thrift
+thrift.exe --gen py -out obs-thrift-api/tests/ obs_thrift_server.thrift
+thrift.exe --gen java -out java/ obs_thrift_server.thrift
