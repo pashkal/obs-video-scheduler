2cc9097b7e7e6e2d6b0608b6d5af071f3b6337c9
Pavel Krotkov <krotkov.pavel@gmail.com>
2020-04-05 13:08:36 -0700

    Add web app

--- gen.bat
+++ gen.bat
@@ -1,3 +1,3 @@
 thrift.exe --gen cpp:no_skeleton -out obs-thrift-api/obs-thrift-api/src/ obs_thrift_server.thrift
 thrift.exe --gen py -out obs-thrift-api/tests/ obs_thrift_server.thrift
-thrift.exe --gen java -out java/ obs_thrift_server.thrift
+thrift.exe --gen java -out obs-video-scheduler/src/ obs_thrift_server.thrift
