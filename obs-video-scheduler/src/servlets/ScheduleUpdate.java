2b8d5327485b130f33d6787597668c0648fce3ad
Pavel Krotkov <krotkov.pavel@gmail.com>
2020-07-12 14:09:14 -0700

    Fix scheduling of videos with & in name

--- obs-video-scheduler/src/servlets/ScheduleUpdate.java
+++ obs-video-scheduler/src/servlets/ScheduleUpdate.java
@@ -35,9 +35,7 @@ public class ScheduleUpdate extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String newSchedule = request.getParameter("schedule");
-		
 		DataProvider.updateSchedule(newSchedule);
-		
 	}
 
 	/**
