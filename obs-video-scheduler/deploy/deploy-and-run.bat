bb139322865f1b402a862a127253131ec3969702
Pavel Krotkov <krotkov.pavel@gmail.com>
2020-07-12 12:24:45 -0700

    Fix deploy-and-run to work with relative paths

--- obs-video-scheduler/deploy/deploy-and-run.bat
+++ obs-video-scheduler/deploy/deploy-and-run.bat
@@ -1,38 +1,44 @@
+# This file is intended for web application development workflow
+# Run install.ps1 once to deploy initial version of scheduler in this folder
+
+# Use deploy-and-run.bat to copy over changed *.class files and other web-app 
+# related files when doing development
+
+# Some extensions or dirs might be missing!
+
 pushd apache-tomcat-9.0.33\webapps\ROOT
 
 pushd WEB-INF\classes
 pushd scheduler
 del *.class
-copy d:\git\obs-video-scheduler\obs-video-scheduler\target\classes\scheduler\*.class .
+copy ..\..\..\..\..\..\..\target\classes\scheduler\*.class .
 popd
 pushd services
 del *.class
-copy d:\git\obs-video-scheduler\obs-video-scheduler\target\classes\services\*.class .
+copy ..\..\..\..\..\..\..\target\classes\services\*.class .
 popd
 pushd servlets
 del *.class
-copy d:\git\obs-video-scheduler\obs-video-scheduler\target\classes\servlets\*.class .
+copy ..\..\..\..\..\..\..\target\classes\servlets\*.class .
 popd
 pushd util
 del *.class
-copy d:\git\obs-video-scheduler\obs-video-scheduler\target\classes\util\*.class .
+copy ..\..\..\..\..\..\..\target\classes\util\*.class .
 popd
 popd
 
 pushd css
 del *.css
-copy D:\git\obs-video-scheduler\obs-video-scheduler\WebContent\*.css .
+copy ..\..\..\..\..\WebContent\css\*.css .
 popd
 
 
 del index.html
-copy D:\git\obs-video-scheduler\obs-video-scheduler\WebContent\index.html .
+copy ..\..\..\..\WebContent\index.html .
 
-del settings.jsp
-copy D:\git\obs-video-scheduler\obs-video-scheduler\WebContent\settings.jsp .
-
-del OBSStatus.jsp
-copy D:\git\obs-video-scheduler\obs-video-scheduler\WebContent\OBSStatus.jsp .
+del *.jsp
+copy ..\..\..\..\WebContent\*.jsp .
+popd
 
 popd
 run.bat
