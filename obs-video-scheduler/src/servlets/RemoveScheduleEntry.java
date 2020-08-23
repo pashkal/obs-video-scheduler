9f338aae5d09940d78d1ae7c0369ab305aaf7094
Pavel Krotkov <krotkov.pavel@gmail.com>
2020-08-23 15:17:30 -0700

    Make deletions go through server

---
+++ obs-video-scheduler/src/servlets/RemoveScheduleEntry.java
@@ -0,0 +1,68 @@
+package servlets;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.DataProvider;
+import util.Item;
+import util.ScheduleEntry;
+
+/**
+ * Servlet implementation class VideoList
+ */
+@WebServlet("/RemoveScheduleEntry")
+public class RemoveScheduleEntry extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor.
+     * 
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    public RemoveScheduleEntry() throws FileNotFoundException, IOException {
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String uuid = request.getParameter("uuid");
+        
+        List<ScheduleEntry> schedule = DataProvider.getSchedule();
+        schedule.removeIf(entry -> entry.id == Long.parseLong(uuid));
+        
+        DataProvider.updateSchedule(schedule);
+        
+        DataProvider.writeScheduleToClient(response, schedule);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
