827fdefb2057b11c9d6d549b07664226c10a5dcd
Pavel Krotkov <krotkov.pavel@gmail.com>
2020-08-23 16:31:49 -0700

    Move launching videos into ScheduleEntry

--- obs-video-scheduler/src/services/VideoLaunchService.java
+++ obs-video-scheduler/src/services/VideoLaunchService.java
@@ -1,18 +1,14 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
 
-import util.Config;
 import util.DataProvider;
-import util.Disclaimer;
 import util.ScheduleEntry;
 import util.Item;
-import util.OBSApi;
 
 public class VideoLaunchService implements Runnable {
 
@@ -34,34 +30,10 @@ public class VideoLaunchService implements Runnable {
                 Map<String, Item> items = DataProvider.getAllItemsByName();
                 long time = new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000;
                 for (ScheduleEntry e : schedule) {
-                    try {
-                        if (!items.get(e.itemName).isVideo)
-                            continue;
-                        long start = e.start;
-                        long stop = e.start + items.get(e.itemName).duration + Disclaimer.getDuration();
-                        if (time > start - 2000 && time < start) {
-                            Thread.sleep(start - time);
-                            sc.log("Launching " + e.itemName);
-                            System.err.println("Launching " + e.itemName);
-
-                            new OBSApi().launchVideo(e.itemName);
-
-                            Thread.sleep(1500);
-                        }
-                        if (time > stop - 2000 && time < stop) {
-                            Thread.sleep(stop - time);
-                            System.err.println("Stopping " + e.itemName);
-                            new OBSApi().removeScheduledVideo();
-                            Thread.sleep(1500);
-                        }
-
-                    } catch (Exception ee) {
-                        ee.printStackTrace();
-                        System.err.println(e.toString());
-                    }
+                    e.process(items, time);
                 }
-            } catch (Exception e1) {
-                e1.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
             try {
                 Thread.sleep(1000);
--- obs-video-scheduler/src/util/ScheduleEntry.java
+++ obs-video-scheduler/src/util/ScheduleEntry.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.Map;
+
 import javax.json.Json;
 import javax.json.JsonValue;
 
@@ -23,5 +25,33 @@ public class ScheduleEntry {
     public JsonValue toJsonValue() {
         return Json.createObjectBuilder().add("uuid", this.uuid).add("start_timestamp", this.start).add("name", itemName)
                 .build();
+    }
+    
+    public void process(Map<String, Item> allItems, long time) {
+        try {
+            if (!allItems.get(this.itemName).isVideo)
+                return;
+            long start = this.start;
+            long stop = this.start + allItems.get(this.itemName).duration + Disclaimer.getDuration();
+            
+            if (time > start - 2000 && time < start) {
+                Thread.sleep(start - time);
+                System.err.println("Launching " + this.itemName);
+
+                new OBSApi().launchVideo(this.itemName);
+
+                Thread.sleep(1500);
+            }
+            
+            if (time > stop - 2000 && time < stop) {
+                Thread.sleep(stop - time);
+                System.err.println("Stopping " + this.itemName);
+                new OBSApi().removeScheduledVideo();
+                Thread.sleep(1500);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.toString());
+        }
     }
 }
